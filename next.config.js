8b976ab6dc801737b57b4ba276cb914c0524d7d7
Evgeny Shenets <evgenyshenets91@gmail.com>
2023-02-22 16:03:54 +0400

    revert next.config.js

--- next.config.js
+++ next.config.js
@@ -1,27 +1,6 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
-
-const nextConfig = phase => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      reactStrictMode: true,
-      env: {
-        mongodb_username: 'dbUser',
-        mongodb_password: 'EDQV5MHGEQUjbRZW',
-        mongodb_clustername: 'cluster0',
-        mongodb_database: 'contact-form-development',
-      },
-    };
-  }
-
-  return {
-    reactStrictMode: true,
-    env: {
-      mongodb_username: process.env.NEXT_PUBLIC_mongodb_username,
-      mongodb_password: process.env.NEXT_PUBLIC_mongodb_password,
-      mongodb_clustername: process.env.NEXT_PUBLIC_mongodb_clustername,
-      mongodb_database: process.env.NEXT_PUBLIC_mongodb_database,
-    },
-  };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
