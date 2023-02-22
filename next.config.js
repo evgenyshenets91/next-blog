70b27215cb22227fef3baacaf4e6b4368534430f
Evgeny Shenets <evgenyshenets91@gmail.com>
2023-02-22 15:53:13 +0400

    change config with NEXT_PUBLIC_

--- next.config.js
+++ next.config.js
@@ -16,10 +16,10 @@ const nextConfig = phase => {
   return {
     reactStrictMode: true,
     env: {
-      mongodb_username: 'dbUser',
-      mongodb_password: 'EDQV5MHGEQUjbRZW',
-      mongodb_clustername: 'cluster0',
-      mongodb_database: 'contact-form',
+      mongodb_username: process.env.NEXT_PUBLIC_mongodb_username,
+      mongodb_password: process.env.NEXT_PUBLIC_mongodb_password,
+      mongodb_clustername: process.env.NEXT_PUBLIC_mongodb_clustername,
+      mongodb_database: process.env.NEXT_PUBLIC_mongodb_database,
     },
   };
 };
